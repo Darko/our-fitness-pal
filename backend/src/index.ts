397b34c57d1076392dbe0893a893206b3632b802
Darko <espadomkd@gmail.com>
2019-06-11 01:06:29 +0200

    Setup graphql

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,19 @@
 import app from './app';
 import config from './config';
+import graphqlHttp from 'express-graphql';
+import { gqlSchema } from './models/schema';
 import './mongo';
 import './routes';
 
+app.use(
+  '/gql',
+  graphqlHttp({
+    schema: gqlSchema,
+    graphiql: true
+  })
+);
+
 app.listen(config.port, () => {
   console.clear();
-  console.log(`Example app listening on port ${config.port}!`)
+  console.log(`Example app listening on port http://localhost:${config.port}!`);
 });
--- backend/src/models/schema.ts
+++ backend/src/models/schema.ts
@@ -1,11 +1,42 @@
 import mongoose, { Schema, Document } from 'mongoose';
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLSchema
+} from 'graphql';
 
-export interface SomeInterface extends Document, IAccount {
-  memes: boolean;
+interface GoodMeme {
+  id: number;
+  name: string;
 }
 
-const SomeSchema: Schema = new Schema({
-  memes: { type: Boolean, default: true }
+const db: GoodMeme[] = [{ id: 1, name: 'Pepe the Frog' }];
+
+const MemeType = new GraphQLObjectType({
+  name: 'meme',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString }
+  })
 });
 
-export default mongoose.model<SomeInterface>('SomeSchema', SomeSchema);
+export const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    meme: {
+      type: MemeType,
+      args: { id: { type: GraphQLID } },
+      resolve: (parent, args) => {
+        console.log(args);
+        return db.find(meme => {
+          return meme.id === Number(args.id);
+        });
+      }
+    }
+  }
+});
+
+export const gqlSchema = new GraphQLSchema({
+  query: RootQuery
+});
