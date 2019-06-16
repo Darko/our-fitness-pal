da97c542959cffecb00215a5f3dd04e5032056b4
Darko <espadomkd@gmail.com>
2019-06-17 01:28:34 +0200

    Content to work with

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,27 @@ import * as React from 'react';
 import './App.css';
 
 const App: React.FunctionComponent = () => {
-  return <div>xd</div>;
+  return (
+    <>
+      <h3>Gains</h3>
+
+      <p>Next workout:</p>
+      <h5>Push</h5>
+
+      <ul>
+        <li>Bench press: 2 warmup, 3 working sets 6-8 reps</li>
+        <li>Incline Cable Fly: 4 sets, 15 reps</li>
+        <li>Standing dumbbell shoulder press: 3 sets, 15 reps</li>
+        <li>Egyptian cable lateral shoulder rise: 3 sets, 15 reps</li>
+        <li>Triceps rope extensions: 3 sets, 15 reps</li>
+      </ul>
+
+      <h5>Statistics</h5>
+      <p>5% increase in moved weight</p>
+      <p>Last week: 150kg</p>
+      <p>This week: 157kg</p>
+    </>
+  );
 };
 
 export default App;
